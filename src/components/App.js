4e331944630b4abc611b2d9e952a6cbb01bf4636
rishawraj20 <120494559+rishawraj20@users.noreply.github.com>
2023-04-09 11:49:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
-mport React, { useState } from 'react'
+
+ 
+
+import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
@@ -65,4 +68,11 @@ const App = () => {
 }
 
 
-export default App
+export default App;
+  
+
+
+
+
+
+
